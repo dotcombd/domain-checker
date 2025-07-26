1918f316037cdba2d8ec4e38675d6fd0253aae53
dotcombd <sobujsfs@gmail.com>
2025-07-26 23:39:11 +0600

    Create form-template.php

---
+++ templates/form-template.php
@@ -0,0 +1,9 @@
+<div class="bd-domain-wrapper">
+    <form id="bdDomainCheckerForm">
+        <div class="domain-input-group">
+            <input type="text" id="domainInput" placeholder="Enter domain name (without extension)">
+            <button type="submit">Check Domain</button>
+        </div>
+    </form>
+    <div id="bdResultContainer"></div>
+</div>
